2b50e85870c12af460a4124ff56e5eda52e2f670
vtutti <76520252+vtutti@users.noreply.github.com>
2021-03-19 01:09:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+  include("connection.php");
+  include("functions.php");
+
+$user_data = check_login($con);
+ 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>HeartCheck</title>
+</head>
+<body>
+<a href="logout.php"> Logout</a>
+<h1> Login for heartcheck </h1>
+<br>
+Welcome, username
+
+
+</body>
+</html>
